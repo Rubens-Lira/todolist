4142afb662ce05be867d818800d20cc72ab1abd0
Rubens-Lira <rubenslira1156@gmail.com>
2025-04-04 13:22:31 -0300

    Adicionado as funções de validação para email e telefone

--- api/src/utils/index.js
+++ api/src/utils/index.js
@@ -1,11 +1,20 @@
 export function formatFullDate(date) {
   const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0"); 
-  const year = date.getFullYear(); 
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`; 
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
 
+export function emailIsValid(email) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+
+export function phoneIsValid(phone) {
+  const regex = /^\d{10,11}$/;
+  return regex.test(phone);
+}
